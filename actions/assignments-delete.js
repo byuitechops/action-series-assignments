57d61fb83302e881e71a332ed3e9b73e317597b6
Sam McGrath <mcg13004@byui.edu>
2018-03-02 10:21:56 -0700

    Completed grandchild module

---
+++ actions/assignments-delete.js
@@ -0,0 +1,35 @@
+module.exports = (course, assignment, callback) => {
+    course.message(`Analyzing ${assignment.name}`);
+    
+    //if delete attribute is already set to true, do nothing
+    if (assignment.techops.delete) {
+        callback(null, course, assignment);
+        return;
+    }
+
+    //insert assignments to delete here
+    var doomedAssignments = [
+        /\[co~\d*\]/i   //delete Course Outcomes assignments
+    ];
+
+    var found = doomedAssignments.find((rgx) => rgx.test(assignment.name));
+
+    //modify assignment properties to prep it for termination
+    //only called found is NOT undefined
+    function modifyAssignment() {
+        assignment.techops.delete = true;
+        course.log('Assignments - Deleted', {
+            'Title': assignment.name,
+            'ID': assignment.id
+        });
+    }
+
+    //an assignment matching at least one element in the 
+    //doomedAssignments has been found.
+    if (typeof found != "undefined") {
+        modifyAssignment();
+    }
+
+    //return and report
+    callback(null, course, assignment);
+};
