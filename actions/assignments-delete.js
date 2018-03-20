cfbeecd78204e9beef2a61ecaef465ad79fd0f67
Seth Childers <sethchilders92@gmail.com>
2018-03-20 13:30:33 -0600

    Small changes

--- actions/assignments-delete.js
+++ actions/assignments-delete.js
@@ -7,28 +7,34 @@ module.exports = (course, assignment, callback) => {
 
     //insert assignments to delete here
     var doomedAssignments = [
-        /\[co~\d*\]/i   //delete Course Outcomes assignments
+        /\[co~\d*\]/i //delete Course Outcomes assignments
     ];
 
     var found = doomedAssignments.find((rgx) => rgx.test(assignment.name));
 
     //modify assignment properties to prep it for termination
     //only called found is NOT undefined
-    function modifyAssignment() {
-        assignment.techops.delete = true;
+    function action() {
+        var logCategory = 'Assignments - Deleted';
 
-        assignment.techops.log('Assignments - Deleted', {
+        /* If we're running a standards check and not doing any changes... */
+        if (course.info.checkStandard === true) {
+            logCategory = 'Assigments - Deprecated';
+        } else {
+            assignment.techops.delete = true;
+        }
+
+        assignment.techops.log(logCategory, {
             'Title': assignment.name,
             'ID': assignment.id
         });
+        callback(null, course, assignment);
     }
 
-    //an assignment matching at least one element in the 
-    //doomedAssignments has been found.
-    if (typeof found != 'undefined') {
-        modifyAssignment();
+    //an assignment matching at least one element in the doomedAssignments has been found.
+    if (found != undefined) {
+        action();
+    } else {
+        callback(null, course, assignment);
     }
-
-    //return and report
-    callback(null, course, assignment);
 };
